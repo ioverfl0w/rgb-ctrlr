94e0864c13a1037d7a15eb60fe98df9fed540d0a
ioverfl0w <ioverfl0w@users.noreply.github.com>
2022-08-08 18:50:37 -0400

    added pulse, rainbow patterns

--- rgbctrl_mega.ino
+++ rgbctrl_mega.ino
@@ -3,8 +3,8 @@
   Control a 4-wire RGB device, using a button to change colors and patterns
 
 */
-int selectedColor = 7;
-int selectedPattern = 0;
+int selectedColor = 0;
+int selectedPattern = 2;
 
 static int buttonPin = A8;
 static int buttonHold = 500; // time duration in ms to declare button held
@@ -12,17 +12,19 @@ static int defaultDelay = 50;
 static int groups[][3] = {
   {3, 6, 5}
 };
-static int patterns[][1] {
-  800 //Constant on
+static int patterns[] {
+  500,  //Constant on
+  60,   //Pulse
+  60,   //Rainbow Fade
 };
 static int colors[][3] {
-  {255, 0,   0}, //red
-  {0,   255, 0}, //green
-  {0,   0,   255}, //blue
-  {255, 255, 255}, //white
-  {255, 255, 0}, //yellow
-  {255, 0,   255}, //purple
-  {0,   255, 255}, //cyan
+  {255, 0,   0},    //red
+  {0,   255, 0},    //green
+  {0,   0,   255},  //blue
+  {255, 255, 255},  //white
+  {255, 255, 0},    //yellow
+  {255, 0,   255},  //purple
+  {0,   255, 255},  //cyan
 };
 static int groupsLen = (sizeof(groups) / 3) / sizeof(int);
 static int patternsLen = sizeof(patterns) / sizeof(int);
@@ -40,7 +42,9 @@ void setup() {
 void loop() {
   performPattern();
   buttonWork();
-  debug();
+  if (debug) {
+    debug();
+  }
   delay(patterns[selectedPattern]);
 }
 
@@ -49,27 +53,84 @@ void performPattern() {
     case 0: // Constant on
       activateColor();
       break;
+    case 1: // Pulse
+      pulse();
+      break;
+    case 2: // Rainbow Fade
+      rainbow();
+      break;
   }
+}
+
+static int pulseIncr = 12;
+boolean pulseAscend = true;
+int pulseBlend[16][3];
+int pulsePosition = 0;
+int pulseColor = -1;
+void pulse() {
+  //setup
+  if (pulseColor != selectedColor) {
+    pulseColor = selectedColor;
+    for (int i = 0; i < pulseIncr; i++) {
+      for (int x = 0; x < 3; x++) {
+        pulseBlend[i][x] = (colors[selectedColor][x] / pulseIncr) * i;
+      }
+    }
+  }
+
+  activateColor(pulseBlend[pulsePosition]);
+  if (pulseAscend) {
+    if ((pulsePosition + 1) == pulseIncr) {
+      pulseAscend = false;
+    } else {
+      pulsePosition++;
+    }
+  } else {
+    if (pulsePosition == 1) { // set to 1, so light never goes completely off
+      pulseAscend = true;
+    } else {
+      pulsePosition--;
+    }
+  }
+}
+
+static boolean rainbowRandom = true;
+boolean rainbowSwitch = false;
+void rainbow() {
+  if (!pulseAscend && !rainbowSwitch) {
+    rainbowSwitch = true;
+  }
+  if (pulseAscend && rainbowSwitch) {
+    if (rainbowRandom) {
+      randomColor();
+    } else {
+      nextColor();
+    }
+    rainbowSwitch = false;
+  }
+  pulse();
 }
 
 void buttonWork() {
   int btnStatus = digitalRead(buttonPin);
 
   if (btnStatus == HIGH) {
-    Serial.println("button triggered .. ");
+    if (debug) {
+      Serial.println("button triggered .. ");
+    }
     delay(buttonHold - defaultDelay);
 
     // button was held
     if (btnStatus == digitalRead(buttonPin)) {
-      Serial.println("button was held .. change color");
+      Serial.println("button was held .. change color"); // DEBUG
       //change color
-      selectedColor = ((selectedColor + 1) == colorsLen) ? 0 : selectedColor + 1;
+      nextColor();
       activateColor();
       delay(500); // time to let go of button
 
       //button was not held for the set duration
     } else {
-      Serial.println("button was NOT held .. change pattern");
+      Serial.println("button was NOT held .. change pattern"); // DEBUG
       //change pattern
       selectedPattern = ((selectedPattern + 1) == patternsLen) ? 0 : selectedPattern + 1;
     }
@@ -78,9 +139,31 @@ void buttonWork() {
   }
 }
 
+void randomColor() {
+  int p = selectedColor;
+  selectedColor = random(colorsLen - 1);
+  if (selectedColor == p) { // prevent two in a rows
+    return randomColor();
+  }
+}
+
+void nextColor() {
+  selectedColor = ((selectedColor + 1) == colorsLen) ? 0 : selectedColor + 1;
+}
+
+void prevColor() {
+  selectedColor = ((selectedColor - 1) == -1) ? colorsLen - 1 : selectedColor - 1;
+}
+
 void activateColor() {
   for (int i = 0; i < groupsLen; i++) {
     activateColor(i);
+  }
+}
+
+void activateColor(int color[]) {
+  for (int i = 0; i < groupsLen; i++) {
+    activateColor(i, color);
   }
 }
 
@@ -90,14 +173,8 @@ void activateColor(int grp) {
 
 void activateColor(int grp, int color[]) {
   for (int i = 0; i < 3; i++) {
-    digitalWrite(groups[grp][i], color[i]);
-    Serial.print(" - pin: ");
-    Serial.print(groups[grp][i]);
-    Serial.print(" - color: ");
-    Serial.println(color[i]);
-    Serial.println();
+    analogWrite(groups[grp][i], color[i]);
   }
-  Serial.println("-----");
 }
 
 void debug() {
